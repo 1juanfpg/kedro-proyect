d3e6b3b80e200d2038c419d42f8a69a889428039
juan felipe <jfpadillag@uqvirtual.edu.co>
2022-10-05 00:31:47 -0500

    update etl

--- src/kedro_phone/pipelines/etl/nodes.py
+++ src/kedro_phone/pipelines/etl/nodes.py
@@ -18,10 +18,10 @@ from deepchecks.tabular import Dataset
 from deepchecks.tabular.suites import data_integrity
 from deepchecks.tabular.suites import train_test_validation
 
-# import great_expectations as ge
-# from great_expectations.checkpoint.types.checkpoint_result import CheckpointResult
+import great_expectations as ge
+from great_expectations.checkpoint.types.checkpoint_result import CheckpointResult
 
-# from feature_engine.selection import DropConstantFeatures, DropDuplicateFeatures
+from feature_engine.selection import DropConstantFeatures, DropDuplicateFeatures
 
 logger = logging.getLogger(__name__)
 
@@ -31,4 +31,177 @@ def load_data(parameters: Dict[str, Any]) -> pd.DataFrame:
         parameters['path']: path to data from.
     Returns: dataframe containing data.
     """
-    return pd.read_csv(parameters['path'])
+    data = pd.read_csv(parameters['path'])
+
+    # print(data.info())
+
+    return data
+
+def etl_processing(data: pd.DataFrame, parameters: Dict[str, Any]) -> pd.DataFrame:
+    """
+    General transformations to the data like removing columns with
+    the same constant value, duplicated columns., duplicate values
+
+    Args:
+        data: raw data after extract
+        parameters: list of the general transforms to apply to all the data
+
+    Returns:
+        pd.DataFrame: transformed data
+
+    """
+    # print (data.info())
+    mlflow.set_experiment('price_range')
+    mlflow.log_param("shape raw_data", data.shape)
+
+    data = (data
+            .pipe(clean_data)
+            .pipe(drop_duplicates, drop_cols=['index'])
+            .dropna(inplace=True,axis=0)
+    )
+
+    # convert this step as a scikit-learn transformer
+    # this steps is only useful until at the end in the SLQ query or
+    # data load method, only query the specific columns
+    # the specific columns
+    columns = parameters['features']
+    # convert target column (str) to list
+    target = parameters['target_column']
+    columns.append(target)
+
+
+    pipe_functions = [
+        ('drop_constant_values', DropConstantFeatures(tol=1, missing_values= 'ignore')),
+        ('drop_duplicates', DropDuplicateFeatures(missing_values= 'ignore'))                
+    ]
+
+    # get methods name for experimentation tracking
+    methods = []
+    for name, _ in pipe_functions:
+        methods.append(name)
+
+    mlflow.set_experiment('price_range')
+    mlflow.log_param('etl_transforms', methods)
+
+    print(methods)
+
+    pipeline_train_data = Pipeline(steps=pipe_functions)
+    print(pipeline_train_data)
+
+    #apply transformation to data
+    data_transformed = pipeline_train_data.fit_transform(data)
+    print(data_transformed)
+
+    mlflow.log_param("shape data etl", data_transformed.shape)
+
+    return data_transformed
+
+# --- Node ---
+
+# integridad de los datos 
+def data_integrity_validation(data: pd.DataFrame, parameters: Dict) -> pd.DataFrame:
+
+    categorical_features = parameters['categorical_cols']
+    label = parameters['target_column']
+
+    dataset = Dataset(data,
+                 label=label,
+                 cat_features=categorical_features)
+
+    # Run Suite:
+    integ_suite = data_integrity()
+    suite_result = integ_suite.run(dataset)
+
+    mlflow.set_experiment('price_range')
+    mlflow.log_param(f"data integrity validation", str(suite_result.passed()))
+    
+    if not suite_result.passed():
+        # save report in data/08_reporting
+        suite_result.save_as_html('data/08_reporting/data_integrity_check.html')
+        logger.error("data integrity not pass validation tests")
+        #raise Exception("data integrity not pass validation tests")
+    return data
+
+# separación de los datos en test y train
+def split_data(data: pd.DataFrame, parameters: Dict) -> Tuple:
+    """Splits data into features and targets training and test sets.
+    Args:
+        data: Data containing features and target.
+        parameters: Parameters defined in parameters/data_processing.yml.
+    Returns:
+        Split data.
+    """
+    mlflow.set_experiment('price_range')
+    mlflow.log_param("split random_state", parameters['split']['random_state'])
+    mlflow.log_param("split test_size", parameters['split']['test_size'])
+
+    #remove rows without target information
+    data = data.dropna(subset=[parameters['target_column']])
+
+    x_features = data[parameters['features']]
+    y_target = data[parameters['target_column']]
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_features,
+        y_target,
+        test_size=parameters['split']['test_size'],
+        random_state=parameters['split']['random_state']
+    )
+
+    mlflow.log_param(f"shape train", x_train.shape)
+    mlflow.log_param(f"shape test", x_test.shape)
+
+    return x_train, x_test, y_train, y_test
+
+# Validación de los datos de salida 
+def train_test_validation_dataset(x_train,
+                                  x_test,
+                                  y_train,
+                                  y_test,
+                                  parameters: Dict) -> Tuple:
+    categorical_features = parameters['categorical_cols']
+    label = parameters['target_column']
+
+    train_df = pd.concat([x_train, y_train], axis=1)
+    test_df = pd.concat([x_test, y_test], axis=1)
+
+    train_ds = Dataset(train_df,
+                       label=label,
+                       cat_features=categorical_features
+                       )
+    test_ds = Dataset(test_df,
+                      label=label,
+                      cat_features=categorical_features
+                      )
+    validation_suite = train_test_validation()
+    suite_result = validation_suite.run(train_ds, test_ds)
+    
+    mlflow.set_experiment('price_range')
+    mlflow.log_param("train_test validation", str(suite_result.passed()))
+    
+    if not suite_result.passed():
+        # save report in data/08_reporting
+        suite_result.save_as_html('data/08_reporting/train_test_check.html')
+        logger.error("Train / Test Dataset not pass validation tests")
+    return x_train, x_test, y_train, y_test
+
+# --- help functions -------
+
+# funcion para le limpiado de datos
+def clean_data(data: pd.DataFrame) -> pd.DataFrame:
+    """ Replace target column to 1 and 0 values"""
+    data.replace('nhbgvfrtd 56gyub', np.nan, inplace=True)
+    data.replace('??????', np.nan, inplace=True)
+    data.replace('nan', np.nan, inplace=True)
+    data.replace('-948961565145.0', np.nan, inplace=True)
+    data.replace('5285988458456.0', np.nan, inplace=True)
+    print("Replace data invalid")
+    return data
+
+# remove duplicates from data based on a column
+def drop_duplicates(data: pd.DataFrame,
+                    drop_cols: list) -> pd.DataFrame:
+    """Drop duplicate rows from data."""
+    data = data.drop_duplicates(subset=drop_cols, keep='first')
+    print("Delete data duplicate")
+    return data
--- src/kedro_phone/pipelines/etl/pipeline.py
+++ src/kedro_phone/pipelines/etl/pipeline.py
@@ -11,8 +11,53 @@ def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([])
 
 
-from .nodes import (split_data,
+from .nodes import (
                     load_data,
-                    etl_processing, validation_data,
-                    train_test_validation_dataset, data_integrity_validation
+                    etl_processing, 
+                    data_integrity_validation,
+                    split_data,
+                    train_test_validation_dataset
                     )
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+               func=load_data,
+               inputs="parameters",
+               outputs="Data_raw",
+               name="get_data_raw",
+            ),
+            node(
+                func=etl_processing,
+                inputs=["Data_raw", "parameters"],
+                outputs="data_preprocessed",
+                name="etl_transforms",
+            ),
+            node(
+                func=data_integrity_validation,
+                inputs=["data_preprocessed","parameters"],
+                outputs="data_integrity_check",
+                name="data_integrity_validation",
+            ),
+            node(
+                func=split_data,
+                inputs=["data_integrity_check", "parameters"],
+                outputs=["x_train_split",
+                         "x_test_split",
+                         "y_train_split",
+                         "y_test_split"],
+                name="split-train_test",
+            ),
+            node(
+                func=train_test_validation_dataset,
+                inputs=["x_train_split",
+                        "x_test_split",
+                        "y_train_split",
+                        "y_test_split",
+                        "parameters"],
+                outputs=["x_train", "x_test", "y_train", "y_test"],
+                name="validation-split-train_test",
+            )
+        ]
+    )
